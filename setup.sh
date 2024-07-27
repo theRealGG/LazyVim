185df972cad260852eacc2739556f295cea4c907
Hisham Görner <hisham.goerner@silbury.com>
2024-07-27 18:09:49 +0200

    Updated config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env sh
 
+NVIM_DIR="$HOME/.config/nvim"
+
 echo "Installing and setting additional dependencies ..."
+
+echo "CHECK required dependencies"
+
+if ! command -v nvim >/dev/null 2>&1; then
+	echo "Neovim not found install it manually"
+	exit 1
+fi
 
 echo "CHECK additional dependencies"
 
@@ -9,4 +18,12 @@ if ! [ -e "$HOME/.local/share/eclipse/lombok.jar" ]; then
 fi
 
 echo "COPY of settings files: "
-cp "$HOME/.config/nvim/formatting/prettierrc.yaml" "$HOME/.prettierrc.yaml"
+cp "$NVIM_DIR/formatting/prettierrc.yaml" "$HOME/.prettierrc.yaml"
+
+echo "POST installation actions:"
+
+if [ -e "$HOME/nvim" ]; then
+	ln -s "$NVIM_DIR" nvim
+else
+	echo "nvim folder already exists skipping ..."
+fi
